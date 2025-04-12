3ac2209ac869a31afb15e63368760736fb0e3b63
saihainath <saiharinathchalavadi@gmail.com>
2025-04-12 12:54:57 +0530

    expense app

---
+++ backend.sh
@@ -0,0 +1,20 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x  | bash
+
+dnf install nodejs -y
+cp backend.servie /etc/systemd/system/backend.service
+
+useradd expense
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+npm install
+
+systemctl daemon-reload
+
+systemctl enable backend
+systemctl start backend
+
+mysql -h <172.31.6.185> -uroot -pExpenseApp@1 < /app/schema/backend.sql
