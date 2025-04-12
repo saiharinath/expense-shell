8b80ace125ac5dc96b10faf9e8354c16d300d04a
saihainath <saiharinathchalavadi@gmail.com>
2025-04-12 13:10:05 +0530

    expense app

--- backend.sh
+++ backend.sh
@@ -13,8 +13,8 @@ unzip /tmp/backend.zip
 npm install
 
 systemctl daemon-reload
-
 systemctl enable backend
 systemctl start backend
 
+dnf install mysql -y
 mysql -h <172.31.6.185> -uroot -pExpenseApp@1 < /app/schema/backend.sql
