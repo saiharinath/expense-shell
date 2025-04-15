efd5553c4c51881f3471bf715dba2aa28a924ef9
saihainath <saiharinathchalavadi@gmail.com>
2025-04-16 00:17:26 +0530

    expense app

--- frontend.sh
+++ frontend.sh
@@ -6,8 +6,9 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -s-o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+# shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
