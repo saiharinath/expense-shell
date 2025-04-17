d387760149520f4257423caf5bc6be3d07b67fcd
saihainath <saiharinathchalavadi@gmail.com>
2025-04-17 11:39:48 +0530

    expense app

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 dnf install nodejs -y
-cp backend.servie /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 rm -rf /app
@@ -19,4 +19,3 @@ systemctl start backend
 
 dnf install mysql -y
 mysql -h 172.31.11.34 -uroot -pExpenseApp@1 < /app/schema/backend.sql
-systemctl restart backend
