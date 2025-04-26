f44d676015b6a94d5d70c012e5607f716c915d61
saihainath <saiharinathchalavadi@gmail.com>
2025-04-26 23:47:58 +0530

    expense app

--- backend.sh
+++ backend.sh
@@ -18,4 +18,4 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-mysql -h 172.31.11.34 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.saiharinath.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
