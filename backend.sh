c329672dd7c65311bc92b6b6346f263ea961c5cd
saihainath <saiharinathchalavadi@gmail.com>
2025-04-29 12:26:37 +0530

    expense app

--- backend.sh
+++ backend.sh
@@ -3,18 +3,24 @@ component=backend
 
 echo Install nodeJS repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
 
 echo Install nodeJS
 dnf install nodejs -y &>>$log_file
+echo $?
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 echo add application user
 useradd expense &>>$log_file
+echo $?
 
 echo clean app content
 rm -rf /app &>>$log_file
+echo $?
+
 mkdir /app
 cd /app
 
@@ -22,14 +28,18 @@ Download_and_extract
 
 echo download dependencies
 npm install &>>$log_file
+echo $?
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+echo $?
 
 echo Install mysql client
 dnf install mysql -y &>>$log_file
+echo $?
 
 echo load schema
 mysql -h mysql.saiharinath.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
--- common.sh
+++ common.sh
@@ -3,8 +3,9 @@ log_file=/tmp/expense.log
 Download_and_extract() {
   echo Download $component code
   curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-
+   echo $?
 
   echo Extracting $component code
   unzip /tmp/$component.zip >>$log_file
+  echo $?
 }
