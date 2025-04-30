ff72024f1822151a4fa1315b4362dc6bcbbfe919
saihainath <saiharinathchalavadi@gmail.com>
2025-04-30 23:23:05 +0530

    expense app

--- backend.sh
+++ backend.sh
@@ -3,19 +3,35 @@ component=backend
 
 echo Install nodeJS repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+ echo FAILED
+fi
 
 echo Install nodeJS
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+ echo FAILED
+fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+ echo FAILED
+fi
 
 echo add application user
 useradd expense &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+ echo FAILED
+fi
 
 echo clean app content
 rm -rf /app &>>$log_file
